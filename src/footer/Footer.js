64c23e0a29d43d24610e7967d269c4b281714249
Владимир Раду <rodny_co@list.ru>
2021-03-21 11:16:12 +0300

    footer start

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,9 +1,14 @@
 import './footer.css';
+import lines from './opodvor-lines.svg';
+import decore from './footer-decor.svg';
 
 function Footer() {
     return (
         <footer className="footer">
-            footer
+            <img className="footer__lines" src={lines} alt='opodvor-lines'></img>
+            <div>info content</div>
+            <img className="footer__decore" src={decore} alt='opodvor-decore'></img>
+
         </footer>
     );
 }
