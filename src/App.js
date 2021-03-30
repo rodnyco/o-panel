b670cffd7d0d06244546a1cc39f94d5156e23703
Владимир Раду <rodny_co@list.ru>
2021-03-30 16:23:39 +0300

    start hello window

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 
+import WelcomeWindow from './WelcomeWindow';
 import Header from './header/Header';
 import './App.css';
 import Body from './body/Body';
 import Footer from './footer/Footer';
 
+const delay = 20;
+
 function App() {
+  const [isActiveApp, setIsActiveApp] = useState({
+    active: false,
+    timer: null
+  });
+
+  const activateApp = () => {
+    clearTimeout(isActiveApp.timer);
+    setIsActiveApp({
+      active: true,
+      timer: setTimeout(() => {
+        console.log('time');
+        setIsActiveApp(false);
+      }, delay * 1000)
+    });
+  }
+
   return (
-    <div className="wrap">
+    <div className="wrap" onClick={activateApp}>
+      <WelcomeWindow show={!isActiveApp.active} />
       <Header/>
       <Body/>
       <Footer/>
--- src/WelcomeWindow.js
+++ src/WelcomeWindow.js
@@ -1,25 +1,6 @@
-import { useState, useEffect } from "react";
-const delay = 5;
-
 function WelcomeWindow(props) {
-    const [show, setShow] = useState(false);
-
-    useEffect(
-        () => {
-            if(props.show) clearTimeout(timer);
-            let timer = setTimeout(() => {
-                setShow(true);
-                console.log(timer);
-            }, delay * 1000);
-            return () => {
-                clearTimeout(timer);
-            };
-        }, [props.time]
-    );
-
-    console.log(props.time);
-    return show ? (
-        <div>show is true, {delay} seconds passed</div>
+    return props.show ? (
+        <div>Hello window true</div>
       ) : (
         null
     );
